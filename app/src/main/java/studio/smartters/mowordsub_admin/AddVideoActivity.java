fb7367382f7e7033d4389a473f9add6f96ad27a8
Ramakanta <33664106+rk472@users.noreply.github.com>
2018-09-07 15:04:16 +0530

    VideoImageDone

--- app/src/main/java/studio/smartters/mowordsub_admin/AddVideoActivity.java
+++ app/src/main/java/studio/smartters/mowordsub_admin/AddVideoActivity.java
@@ -3,13 +3,15 @@ package studio.smartters.mowordsub_admin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddVideoActivity extends AppCompatActivity {
-
+    private EditText etCaption;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_video);
+        etCaption = findViewById(R.id.caption_video);
     }
 
     public void chooseVideo(View view) {
